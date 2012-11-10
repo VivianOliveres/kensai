596901ea5f4b19e360be85f381fda64de472bae7
VivianOliveres <v.oliveres@gmail.com>
2012-11-10 21:26:27 +0100

    MarketServer is now using a SingleThreadExecutor for MarketChannelHandler

--- kensai-market/src/main/java/com/kensai/market/MarketServer.java
+++ kensai-market/src/main/java/com/kensai/market/MarketServer.java
@@ -1,25 +1,48 @@
 package com.kensai.market;
 
+import java.lang.Thread.UncaughtExceptionHandler;
 import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.channel.ChannelFactory;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 public class MarketServer {
 
+	private static final Logger log = LoggerFactory.getLogger(MarketServer.class);
+
+	private static final int SERVER_PORT = 1664;
+
 	public static void main(String[] args) throws Exception {
-		ChannelFactory factory = new NioServerSocketChannelFactory(Executors.newCachedThreadPool(), Executors.newCachedThreadPool());
+		UncaughtExceptionHandler exceptionHandler = new UncaughtExceptionHandler() {
+
+			@Override
+			public void uncaughtException(Thread t, Throwable e) {
+				log.error("Uncaught exception in thread [{}]", t == null ? null : t.getName(), e);
+			}
+		};
+
+		// Set DefaultUncaughtExceptionHandler for all threads
+		Thread.setDefaultUncaughtExceptionHandler(exceptionHandler);
+
+		// Create ChannelFactory which uses only one thread for ChannelHandler
+		ThreadFactory coreThreadFactory = new ThreadFactoryBuilder().setDaemon(false).setNameFormat("CoreThread")
+			.setUncaughtExceptionHandler(exceptionHandler).build();
+		ExecutorService coreExecutor = Executors.newSingleThreadExecutor(coreThreadFactory);
+		ChannelFactory factory = new NioServerSocketChannelFactory(Executors.newCachedThreadPool(), coreExecutor);
 
 		ServerBootstrap bootstrap = new ServerBootstrap(factory);
-
 		bootstrap.setPipelineFactory(new MarketServerChannelPipelineFactory());
-
 		bootstrap.setOption("child.tcpNoDelay", true);
 		bootstrap.setOption("child.keepAlive", true);
-
-		bootstrap.bind(new InetSocketAddress(1664));
+		bootstrap.bind(new InetSocketAddress(SERVER_PORT));
 	}
 
 }
